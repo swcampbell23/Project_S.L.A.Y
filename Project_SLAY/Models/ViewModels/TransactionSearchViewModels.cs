c57be1753230ade810bf58ba4307a0847028f26e
anshumandass <anshuman.das@utexas.edu>
2022-11-29 15:41:12 -0600

    Create TransactionSearchViewModels.cs

---
+++ Project_SLAY/Models/ViewModels/TransactionSearchViewModels.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Project_SLAY.Models.ViewModels
+{
+    public class TransactionSearchViewModels
+    {
+        public TransactionSearchViewModels()
+        {
+        }
+    }
+}
+
