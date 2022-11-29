ab68335c544f34ba14cb48c85f16186c1823dabc
anshumandass <anshuman.das@utexas.edu>
2022-11-29 15:42:43 -0600

    Update TransactionSearchViewModels.cs

--- Project_SLAY/Models/ViewModels/TransactionSearchViewModels.cs
+++ Project_SLAY/Models/ViewModels/TransactionSearchViewModels.cs
@@ -1,11 +1,28 @@
 ﻿using System;
-namespace Project_SLAY.Models.ViewModels
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+
+namespace Project_SLAY.Models
 {
-    public class TransactionSearchViewModels
+    public class SearchViewModel
     {
-        public TransactionSearchViewModels()
-        {
-        }
+        [Display(Name = "Search by Description: ")]
+        public String? Description { get; set; } 
+
+        [Display(Name = "Search by Type: ")]
+        public TransactionType? Type { get; set; }
+
+        //Implement Range Function//
+        [Display(Name = "Search by Amount Range: ")]
+        //[Range(0,1, ErrorMessage = "Quantity must be greater than or equal to zero!")]
+        public Decimal? Amount { get; set; }
+
+        [Display(Name = "Search by Transaction Number: ")]
+        public Int32? TransactionNo { get; set; }
+
+        [Display(Name = "Search by Date: ")]
+        [DisplayFormat(DataFormatString = "{0:MMMM d, yyyy")]
+        public DateTime? Date { get; set; }
     }
 }
-
